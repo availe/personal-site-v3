71360b89340b73910f52346a346b02ed72f334ce
availe <94802525+availe@users.noreply.github.com>
2025-01-27 00:26:00 -0500

    Update project-data.tsx

--- src/data/project-data.tsx
+++ src/data/project-data.tsx
@@ -1,29 +1,29 @@
 export const projectData = [
   {
     title: "Resume Builder V2",
-    description: "Card Description",
+    description: "Second iteration. This time uses a client-side engine as to avoid rate limits. Note slow WiFi speeds may cause the engine to fail to load.",
     videoSrc: "https://www.youtube-nocookie.com/embed/vzwMjmTWf1A?si=PFeaD6tk0nRt1SNC",
     isShort: false,
     buttonLink: "https://resume-builder.availe.io",
     weight: 3,
   },
   {
-    title: "Cool Project",
-    description: "Another awesome project description",
+    title: "C++ Snake",
+    description: "A project I presented in my data structures class. The workings of this have been long lost.",
     videoSrc: "https://www.youtube-nocookie.com/embed/a-3lA6VFp6Q?si=MsDNRgZWCTrBzWd-",
     isShort: true,
     weight: 0,
   },
   {
     title: "Resume Builder V1",
-    description: "Card Description",
+    description: "Fist iteration. Note it is no longer as functional as in the video, as the free API I was relying on appears to be rate limited now.",
     videoSrc: "https://www.youtube-nocookie.com/embed/vrpMLB8Tme4?si=dtukp_VrW4s0TWyt",
     isShort: false,
     weight: 1
   },
   {
     title: "Assembly Counter",
-    description: "Card Description",
+    description: "Built using a PIC18F452 Microcontroller and two 7 segment displays.",
     videoSrc: "https://www.youtube-nocookie.com/embed/zBSE9ReSBCI?si=nP8QfNgOyJ7YQOiL",
     isShort: false,
     weight: 2
