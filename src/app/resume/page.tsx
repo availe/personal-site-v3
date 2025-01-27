12251b0da14307689a6ecb71b68e5948e42d4ea0
availe <94802525+availe@users.noreply.github.com>
2025-01-26 22:32:23 -0500

    resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,8 +1,9 @@
 const Resume = () => {
     return (
-        <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold mt-8">Resume</h1>
-        <p className="text-lg mt-4">This is my resume page.</p>
+        <div className="flex flex-col items-center h-full">
+        <iframe
+            src="/Rafael_Diaz_Resume.pdf#view=FitH&toolbar=0&navpanes=0"
+            className="w-full h-full border-none"        />
         </div>
     );
     }   
